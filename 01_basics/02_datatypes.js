6a07ab9fbfc5c2537414bc5399fa2ead569c6d01
Rupali1104 <166910542+Rupali1104@users.noreply.github.com>
2024-04-28 06:42:41 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as new version
+
+// alert(3+3) //we are using nodejs not browser
+
+console.log(3+3)
+console.log("Rupali")
+// code readability should be high
+
+let name="Rupali"
+let age= 19
+let isLoggedIn= false
+
+// datatypes
+// number => 2 to the power 53
+// bigInt for larger numbers
+// string => " " or '' 
+// boolean => true/false
+// null =>standalone value(empty)
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
