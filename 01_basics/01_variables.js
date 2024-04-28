c4a3a7823097d19d42839309e5a43421037d79e2
Rupali1104 <166910542+Rupali1104@users.noreply.github.com>
2024-04-28 06:18:05 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId=1446727
+let accountEmail="sufufh@gmail.com"
+var accountPassword="736837ty"
+accountCity="jaipur"
+let accountState;
+
+// accountId=2 //not allowed
+
+accountEmail="bgsnhfg@gmail.com"
+accountPassword="334terg"
+accountCity="Bengaluru"
+
+console.log(accountId);
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
+
+// prefer not to use var because of issue in block scope and funmctional scope
