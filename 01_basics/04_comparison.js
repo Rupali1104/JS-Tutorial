6bf4d344c657334b7af3b3053843e28d6331004a
Rupali1104 <166910542+Rupali1104@users.noreply.github.com>
2024-04-28 09:29:13 +0000

    8th video

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// in comparisons both should have same datatypes
+
+// console.log(null > 0); 
+// console.log(null == 0); 
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// avoid above code
+
+// === strict check
+
+console.log("2" == 2);
+console.log("2" === 2);
+
