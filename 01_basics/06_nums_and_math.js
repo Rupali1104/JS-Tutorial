5af66022e456cf80951ece23d436977dab244230
Rupali R <166910542+Rupali1104@users.noreply.github.com>
2024-05-10 11:07:53 +0000

    6th half

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,23 @@
+const score= 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(0));
+
+const othernum = 234.78654477
+
+// console.log(othernum.toPrecision(5));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ============maths=============
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
