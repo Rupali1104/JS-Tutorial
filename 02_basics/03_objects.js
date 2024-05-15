a89a9d56b18209bcb894a92916f4be0d3c9e938d
Rupali R <166910542+Rupali1104@users.noreply.github.com>
2024-05-15 15:56:21 +0000

    obj 1

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -7,16 +7,31 @@ const mySym = Symbol("key1")
 const JsUser={
     name: "Rupali",
     "full name": "Rupali R",
-    [mySym]: "mykey1"
-    age:19,
+    [mySym]: "mykey1",
+    age: 19,         
     location: "Bangalore",
     email: "rupali@gmail.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
 
-console.log(JsUser.email);
-console.log(JsUser["email"]);
-console.log(JsUser["full name"]);
-console.log(JsUser[mySym]);
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
 
+JsUser.email = "rupali67@chat.com"
+// Object.freeze(JsUser)
+JsUser.email = "brghksu@gmail.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS User");
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS User,${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
