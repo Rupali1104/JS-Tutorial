838800e0f564e07662fc9bac5a19c3ca2743f853
Rupali R <166910542+Rupali1104@users.noreply.github.com>
2024-05-20 16:13:16 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,73 @@
+// console.log("R");
+// console.log("U");
+// console.log("P");
+// console.log("A");
+// console.log("L");
+// console.log("I");
+
+function sayMyName(){
+    console.log("R");
+    console.log("U");
+    console.log("P");
+    console.log("A");
+    console.log("L");
+    console.log("I");
+
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+    // let result = number1 + number2
+    // return result 
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3,4)
+
+// console.log("Result: ", result);
+
+function loginUserMessage(username = "sam"){
+    // if(!username){
+    if(username === undefined){
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("Rupali"));
+// console.log(loginUserMessage());
+
+function calculateCartPrice(val1,val2,...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000));
+
+const user = {
+    username: "Rupali",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+   username: "sam",
+   price: 399
+})
+
+const myNewArray = [200, 400, 100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
