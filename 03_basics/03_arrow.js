52aceeff7ffe189b4efa0acb50e65f2a5323b76c
Rupali R <166910542+Rupali1104@users.noreply.github.com>
2024-05-21 17:25:43 +0000

    arrow

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,46 @@
+const user = {
+    username: "Rupali",
+    price: 999,
+
+    welcomeMessage: function(){
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "Rupali"
+//     console.log(this.username);
+// }
+
+// chai()
+
+const chai = () => {
+    let username = "Rupali"
+    console.log(this);
+}
+
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// console.log(addTwo(3, 4))
+
+// const addTwo = (num1, num2) => num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwo = (num1, num2) => ({username: "Rupali"})
+
+
+console.log(addTwo(3, 4))
+
