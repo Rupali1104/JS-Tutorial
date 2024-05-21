5d59d3ca1a2bdada33a8bd3104dae2691189fe57
Rupali R <166910542+Rupali1104@users.noreply.github.com>
2024-05-21 14:09:50 +0000

    scope

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -3,10 +3,47 @@ let a = 300
 if (true){
     let a = 10
     const b = 20
-    console.log("INNER: ",a);
+    // console.log("INNER: ",a);
 }
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "Rupali"
+
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+
+}
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh"){
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+// +++++++++++++ interesting +++++++
+
+console.log(addone(5));
+
+function addone(num){
+     return num + 1
+}
+
+addTwo(5)//error
+const addTwo = function(num){
+    return num + 2
+}
