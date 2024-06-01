70ea51caa4d0d2bfc123ad7d06799ed97a9523ea
Rupali R <166910542+Rupali1104@users.noreply.github.com>
2024-06-01 15:36:56 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,23 @@
+// if
+
+const isUserLoggedIn = true
+const temp = 41
+// if ( temp === 40) {
+//     console.log("less than 50")
+// } else{
+//     console.log("temp is greater than 50");
+// }
+// <,>,>=,<=,==,!=,===,!==
+
+const score = 200 
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power:${power}`);
+// }
+// console.log(`User power:${power}`);
+
+//else if = for many conditions
+
+// && = both should be true
+// || = one can be true
